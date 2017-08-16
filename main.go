0d96502cc71f4f5cc7b3a4b49b1f293b4230193b
Iaroslav Omelianenko <yaric.mail@gmail.com>
2017-08-16 21:19:22 +0300

    Fixed input parsing and main loop execution

--- main.go
+++ main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"github.com/yaricom/stockGO/trade"
 )
 
 func main() {
-	var money float32
+	rand.Seed(157)
+	var money float64
 	var nStocks, daysLeft int
 	engine := trade.New()
-	for daysLeft > 0 {
-		fmt.Scanf("%f %d %d", &money, &nStocks, &daysLeft)
-		inputs := readTradeInputs(nStocks)
-		orders, err := engine.Trade(inputs, money, daysLeft)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			outputTradeOrders(orders)
-		}
+	fmt.Scanf("%f %d %d", &money, &nStocks, &daysLeft)
+	//fmt.Printf("%f %d %d\n", money, nStocks, daysLeft)
+	inputs := readTradeInputs(nStocks)
+	//fmt.Println(len(inputs))
+	orders, err := engine.Trade(inputs, money, daysLeft)
+	//fmt.Println(len(orders))
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		outputTradeOrders(orders)
 	}
 }
 
@@ -25,9 +28,9 @@ func outputTradeOrders(orders []trade.TradeOrder) {
 	fmt.Println(len(orders))
 	for _, order := range orders {
 		if order.Operation == trade.BUY {
-			fmt.Printf("%s %s %d", order.StockName, "BUY", order.Amount)
+			fmt.Printf("%s %s %d\n", order.StockName, "BUY", order.Amount)
 		} else {
-			fmt.Printf("%s %s %d", order.StockName, "SELL", order.Amount)
+			fmt.Printf("%s %s %d\n", order.StockName, "SELL", order.Amount)
 		}
 	}
 }
@@ -35,9 +38,9 @@ func outputTradeOrders(orders []trade.TradeOrder) {
 func readTradeInputs(nStocks int) []trade.TradeInput {
 	inputs := make([]trade.TradeInput, nStocks)
 	for i := 0; i < nStocks; i++ {
-		hist := make([]float32, 5)
+		hist := make([]float64, 5)
 		input := trade.TradeInput{}
-		fmt.Scanf("%s %d %.2f %.2f %.2f %.2f %.2f", &input.StockName, &input.Owned,
+		fmt.Scanf("%s %d %f %f %f %f %f", &input.StockName, &input.Owned,
 			&hist[0], &hist[1], &hist[2], &hist[3], &hist[4])
 		input.History = hist
 
